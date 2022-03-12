aa3a63d7cc1233442c37160ea4da15e2983b59ca
LeoDiasCarvalho <leodiascarvalhosr@gmail.com>
2022-03-11 21:08:39 -0300

    OrderStatus enum

--- src/main/java/com/educandoweb/course/entities/User.java
+++ src/main/java/com/educandoweb/course/entities/User.java
@@ -30,7 +30,7 @@ public class User implements Serializable{
 	
 	@JsonIgnore
 	@OneToMany(mappedBy = "client")
-	private List<Order> orders = new ArrayList();
+	private List<Order> orders = new ArrayList<>();
 	
 	public User() {
 	}
